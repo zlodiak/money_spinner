310b8274675c099fbff8fa05d13525ad1ef11c93
zlodiak <prozaik81-2@yandex.ru>
2017-09-10 20:42:15 +0300

    routing ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SelectComponent } from './select/select.component';
+import { OutputComponent } from './output/output.component';
+
 const routes: Routes = [
   {
     path: '',
-    children: []
-  }
+    redirectTo: '/select',
+    pathMatch: 'full'
+  },
+  {
+    path: 'select',
+    children: [],
+    component: SelectComponent
+  }, 
+  {
+    path: 'output',
+    children: [],
+    component: OutputComponent
+  } 
 ];
 
 @NgModule({
